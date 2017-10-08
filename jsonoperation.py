2c39db4d30073f4c9308ab1c229fce567a6b763e
a <a@sirius>
2017-10-08 20:15:53 +0200

    Fixed getter/setter

--- jsonoperation.py
+++ jsonoperation.py
@@ -2,10 +2,12 @@
 
 try:
     import json
-    from io import StringIO 
-    import decorators
     import jsonschema
     import sys
+
+#project files
+    import economy
+
 
 except ImportError as e:
     print(e, file=sys.stderr)
@@ -38,9 +40,27 @@ class JsonDataInputHadle(DataInputHandle):
 
     SCHEMA="asdf"
 
-    def __init__(self, input_file_name):
+    def __init__(self, input_file_name="", loaded_objects = None):
         DataInputHandle.__init__(input_file_name)
-        self.loaded_objects = []
+ 
+        self.is_extern_load_object = False 
+        self.loaded_objects = loaded_objects       
+
+        if not len(self.input_file_name) and self.loaded_objects is None:
+            raise TypeError("Invalid parameters.")
+
+        if isinstance(loaded_objects, None):
+            self.is_extern_load_object = True
+
+        self.__economy_objects = []
+
+    def __coroutine(self, func):
+        ''' decorator functions for coroutines - initialization '''
+        def ret_func(*args, **kwargs):
+            __func = func(*args, **kwargs)
+            next(__func)
+            return __func
+        return ret_func
 
     @__coroutine
     def __check_schema(self, target, check_schema=False):
@@ -61,13 +81,6 @@ class JsonDataInputHadle(DataInputHandle):
             except GeneratorExit:
                 target.close()
 
-    def __coroutine(self, func):
-        ''' decorator functions for coroutines - initialization '''
-        def ret_func(*args, **kwargs):
-            __func = func(*args, **kwargs)
-            next(__func)
-            return __func
-        return ret_func
 
     @__coroutine
     def __read_file(self, target):
@@ -89,9 +102,18 @@ class JsonDataInputHadle(DataInputHandle):
              exit()
     
     def make_objects(self):
-        self.x = [i for i in self.__open_file(self.__read_file(self.__check_schema(self.load_objects)))] 
+        ''' makes pipe from file: open_file | read_file | load_object > self.loaded_object '''
+        #create pipeline which fill self.loaded_objects
+        if not self.is_extern_load_object:
+            self.__open_file(self.__read_file(self.__check_schema(self.__load_objects))) 
 
-    def load_objects(self):
+        for in_var in self.loaded_objects:
+            tmp_econ = economy.Economy()
+            tmp_econ.__dict__ = in_var
+            self.__economy_objects.insert(tmp_econ)
+
+
+    def __load_objects(self):
         try:
             while True:
                 data = (yield)
@@ -108,10 +130,31 @@ class JsonDataInputHadle(DataInputHandle):
 
         cls.SCHEMA=schema
 
-#TODO Getter/setter    
-    def get_objects(self):
-        pass
-    
-    def set_objects(self):
-        pass
+    @economy_objects.setter
+    def economy_objects(self):
+        return self.__economy_objects
+   
+    @economy_objects.getter
+    def economy_objects(self, value):
+        raise AttributeError("You cannot change this value")
+
+    @extern_status.getter 
+    def extern_status(self):
+        return self.is_extern_load_object
+
+    @extern_status.setter
+    def extern_status(self, status):
+        if not isinstance(status, bool):
+            raise TypeError("Invalid parameters")
+        self.is_extern_loaded_object = status
+
+    @raw_objects.getter
+    def raw_objects(self):
+        return self.loaded_objects
+
+    @raw_objects.setter
+    def raw_objects(self, loaded_objects):
+        self.loaded_objects = loaded_objects
+        self.is_extern_load_object = True
+
 
